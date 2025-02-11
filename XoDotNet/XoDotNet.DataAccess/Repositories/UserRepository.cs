46a367053a2be6b7af4f66537144dc78f267cc01
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2025-02-11 12:34:04 +0300

    Added mongo for users

--- XoDotNet/XoDotNet.DataAccess/Repositories/UserRepository.cs
+++ XoDotNet/XoDotNet.DataAccess/Repositories/UserRepository.cs
@@ -1,10 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
 using XoDotNet.Domain.Abstractions.Repositories;
 using XoDotNet.Domain.Entities;
 
 namespace XoDotNet.DataAccess.Repositories;
 
-public class UserRepository(AppDbContext db) : IUserRepository
+public class UserRepository(AppDbContext db, IMongoCollection<UserRating> userRatingCollection) : IUserRepository
 {
     public async Task<User?> GetUserByUsernameAsync(string username)
     {
@@ -13,18 +14,29 @@ public class UserRepository(AppDbContext db) : IUserRepository
 
     public async Task CreateUser(User user)
     {
-        // TODO: Add rating
         var newUser = await db.Users.AddAsync(user);
         await db.SaveChangesAsync();
+
+        await userRatingCollection.InsertOneAsync(new UserRating
+        {
+            Id = newUser.Entity.Id,
+            Rating = 0,
+            Username = newUser.Entity.Username
+        });
     }
 
     public async Task<UserRating?> GetUserWithRatingByUsernameAsync(string username)
     {
-        throw new NotImplementedException();
+        var userTask = userRatingCollection.Find(user => user.Username == username).FirstOrDefaultAsync();
+        if (userTask == null)
+            return null;
+        return await userTask;
     }
 
     public async Task<List<UserRating>> GetTopRatings(int limit)
     {
-        throw new NotImplementedException();
+        var users = await userRatingCollection.Find(_ => true).SortByDescending(user => user.Rating).Limit(limit)
+            .ToListAsync();
+        return users ?? [];
     }
 }
--- XoDotNet/XoDotNet.DataAccess/ServicesExtensions/AddDatabasesExtensions.cs
+++ XoDotNet/XoDotNet.DataAccess/ServicesExtensions/AddDatabasesExtensions.cs
@@ -1,15 +1,42 @@
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using XoDotNet.DataAccess.Configuration;
 using XoDotNet.DataAccess.Repositories;
 using XoDotNet.Domain.Abstractions.Repositories;
+using XoDotNet.Domain.Entities;
 
 namespace XoDotNet.DataAccess.ServicesExtensions;
 
 public static class AddDatabasesExtensions
 {
-    public static IServiceCollection AddDatabases(this IServiceCollection services)
+    public static IServiceCollection AddDatabases(this IServiceCollection services, MongoDbConfig mongoDbConfig,
+        string postgresConnectionString)
     {
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<IGameRepository, GameRepository>();
+        services.AddSingleton<IMongoDatabase>(_ =>
+        {
+            var mongoClient = new MongoClient(
+                mongoDbConfig.ConnectionString);
+            return mongoClient.GetDatabase(
+                mongoDbConfig.DatabaseName);
+        });
+        services.AddSingleton<IMongoCollection<GameState>>(svc =>
+        {
+            var mongoDatabase = svc.GetService<IMongoDatabase>();
+            if (mongoDatabase == null)
+                throw new Exception("Database not initialized.");
+            return mongoDatabase.GetCollection<GameState>(
+                mongoDbConfig.GameStateCollectionName);
+        });
+        services.AddSingleton<IMongoCollection<UserRating>>(svc =>
+        {
+            var mongoDatabase = svc.GetService<IMongoDatabase>();
+            if (mongoDatabase == null)
+                throw new Exception("Database not initialized.");
+            return mongoDatabase.GetCollection<UserRating>(
+                mongoDbConfig.UserRatingCollectionName);
+        });
         return services;
     }
 }
