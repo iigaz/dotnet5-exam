30ba28ca83c23daf1958e08a422fdb612e754d72
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2025-02-11 23:31:21 +0300

    Fixes on backend

--- XoDotNet/XoDotNet.Features/Games/Commands/CreateGame/CreateGameHandler.cs
+++ XoDotNet/XoDotNet.Features/Games/Commands/CreateGame/CreateGameHandler.cs
@@ -10,10 +10,11 @@ public class CreateGameHandler(IGameRepository gameRepository, IUserRepository u
 {
     public async Task<Result<CreateGameDto>> Handle(CreateGameCommand request, CancellationToken cancellationToken)
     {
+        var userRating = await userRepository.GetUserWithRatingByUsernameAsync(request.Username);
         var user = await userRepository.GetUserByUsernameAsync(request.Username);
-        if (user == null)
+        if (user == null || userRating == null)
             return Result<CreateGameDto>.Failure(new NotFoundError("Could not find username."));
-        var game = await gameRepository.CreateGameAsync(user, Math.Max(0, request.MaxRating));
+        var game = await gameRepository.CreateGameAsync(user, Math.Max(userRating.Rating, request.MaxRating));
         return Result<CreateGameDto>.Success(new CreateGameDto(game.Id));
     }
 }
--- XoDotNet/XoDotNet.GameEvents/Consumers/DeclareWinnerConsumer.cs
+++ XoDotNet/XoDotNet.GameEvents/Consumers/DeclareWinnerConsumer.cs
@@ -8,12 +8,18 @@ using XoDotNet.Mediator;
 
 namespace XoDotNet.GameEvents.Consumers;
 
-public class DeclareWinnerConsumer(IMediator mediator, IGameRepository repository, IGamesHubSender sender)
-    : IConsumer<DeclareWinnerEvent>
+public class DeclareWinnerConsumer(
+    IMediator mediator,
+    IGameRepository repository,
+    IGamesHubSender sender,
+    IMessageScheduler messageScheduler
+) : IConsumer<DeclareWinnerEvent>
 {
     public async Task Consume(ConsumeContext<DeclareWinnerEvent> context)
     {
-        var result = await mediator.Send(new DeclareWinnerCommand(context.Message.GameId, context.Message.Winner));
+        var result = await mediator.Send(
+            new DeclareWinnerCommand(context.Message.GameId, context.Message.Winner)
+        );
         if (result.IsSuccess)
         {
             var game = await repository.GetGameStateByIdAsync(context.Message.GameId);
@@ -22,8 +28,12 @@ public class DeclareWinnerConsumer(IMediator mediator, IGameRepository repositor
                     context.Message.GameId,
                     context.Message.Winner,
                     new GamePlayer(game.Player1.Username, game.Player1.Rating),
-                    new GamePlayer(game.Player2.Username, game.Player2.Rating));
-            await context.SchedulePublish(DateTime.Now.AddSeconds(3), new RestartGameEvent(context.Message.GameId));
+                    new GamePlayer(game.Player2.Username, game.Player2.Rating)
+                );
+            await messageScheduler.SchedulePublish(
+                DateTime.UtcNow.AddSeconds(3),
+                new RestartGameEvent(context.Message.GameId)
+            );
         }
     }
 }
